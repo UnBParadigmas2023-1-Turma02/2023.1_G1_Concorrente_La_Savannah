f12855b0562a719a7c8dafcc72c32233e8a45a4b
lcsAndrade <lucascandradex@protonmail.com>
2023-07-02 13:14:51 -0300

    Parar modelo ao restar um animal

--- main.py
+++ main.py
@@ -19,9 +19,24 @@ class AgentCountElement(TextElement):
 
 
 class SavannaModel(Model):
-  def __init__(self, num_lions = 10, num_gazelles = 10, num_zebras = 10, lion_energy = 25, gazelle_energy = 25, zebra_energy = 25):
+  def __init__(
+    self, 
+    num_lions = 10, 
+    num_gazelles = 10, 
+    num_zebras = 10, 
+    lion_energy = 25, 
+    gazelle_energy = 25, 
+    zebra_energy = 25
+  ):
     self.grid = MultiGrid(GRID_SIZE, GRID_SIZE, torus=True)
     self.schedule = RandomActivation(self)
+    
+    self.num_lions = num_lions
+    self.num_gazelles = num_gazelles
+    self.num_zebras = num_zebras
+    self.lion_energy = lion_energy
+    self.gazelle_energy = gazelle_energy
+    self.zebra_energy = zebra_energy
 
     for i in range(num_lions):
       x = random.randrange(GRID_SIZE)
@@ -46,6 +61,14 @@ class SavannaModel(Model):
 
   def step(self):
     self.schedule.step()
+    if self.one_remaining():
+      self.running = False
+
+  def one_remaining(self):
+    lion_count = sum(isinstance(agent, LionAgent) for agent in self.schedule.agents)
+    gazelle_count = sum(isinstance(agent, GazelleAgent) for agent in self.schedule.agents)
+    zebra_count = sum(isinstance(agent, ZebraAgent) for agent in self.schedule.agents)
+    return (lion_count == 0 and gazelle_count == 0) or (lion_count == 0 and zebra_count == 0) or (gazelle_count == 0 and zebra_count == 0)
 
 
 grid_view = CanvasGrid(lambda agent: agent.get_portrayal(), GRID_SIZE, GRID_SIZE, 500, 500)
